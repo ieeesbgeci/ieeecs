1b02f570cd0fc6fc433705aa5a2c1566a07cb0b2
Code-Theft <56438939+Code-Theft@users.noreply.github.com>
2021-06-10 17:05:47 +0530

    ui-update

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -13,3 +13,25 @@ window.onscroll = function() {myFunction()};
       }
       
     }
+
+   
+$(document).ready(function() {
+    
+      // Whenever the window is scrolled ... 
+      $(window).scroll( function(){
+      
+          // Check the location of the object
+          $('.fade-in').each( function(i){
+              
+              var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              // If the object is inside the window, fade it in!
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+              }
+          }); 
+      });
+  })
+      
